166dd1abfdc660c6fdd5997bff46fa18691038bd
onrtsdmr <onurtaademir@gmail.com>
2020-07-30 13:47:54 +0300

    code improved.

--- ReadExcel/Extensions/Excel.cs
+++ ReadExcel/Extensions/Excel.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using ExcelDataReader;
 using ReadExcel.Models;
 
-namespace ReadExcel
+namespace ReadExcel.Extensions
 {
     public static class Excel
     {
-        public static List<DummyModel> Data = new List<DummyModel>();
+        private static readonly List<DummyModel> Data = new List<DummyModel>();
 
         public static void ReadExcel(this string filePath)
         {
             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
             {
-                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
                 using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
                 {
                     do
                     {
                         while (reader.Read())
-                            Data.Add(new DummyModel()
+                            Data.Add(new DummyModel
                             {
                                 Name = reader.GetString(1),
                                 Price = reader.GetString(2),
-                                Quantity = reader.GetString(3),
+                                Quantity = reader.GetString(3)
                             });
                     } while (reader.NextResult());
                 }
@@ -42,41 +43,3 @@ namespace ReadExcel
         }
     }
 }
-
-/*
-            IExcelDataReader excelReader;
-            List liste = new List();
-            int counter = 0;
- 
-            /*yeni sürümlerde bu kaldırıldığı için kapatıldı.
-            //Datasete atarken ilk satırın başlık olacağını belirtiyor.
-            excelReader.IsFirstRowAsColumnNames = true;
-            DataSet result = excelReader.AsDataSet();*/
-
-//Veriler okunmaya başlıyor.
-/*
-while (excelReader.Read())
-{
-    counter++;
-
-    //ilk satır başlık olduğu için 2.satırdan okumaya başlıyorum.
-    if (counter > 1)
-    {
-        liste.Add("Ad =" + excelReader.GetString(0));
-        liste.Add("Soyad =" + excelReader.GetString(1));
-    }
-}
-
-//Okuma bitiriliyor.
-excelReader.Close();
-
-//Veriler ekrana basılıyor.
-foreach (var item in liste)
-{
-    Console.WriteLine(item);
-}
-
-Console.ReadKey();
-}
-}
-}*/
--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ReadExcel.Extensions;
 
 namespace ReadExcel
 {
